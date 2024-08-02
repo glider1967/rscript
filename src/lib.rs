e125452a0850c03d802d40bf1866cf78d7a75071
glider <glider1967@protonmail.com>
2024-08-03 08:01:19 +0900

    add eval_script

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
+use eval::Eval;
+use parse::Parser;
 use wasm_bindgen::prelude::*;
+
+mod environment;
+mod eval;
+mod expression;
+mod parse;
+mod tokenize;
 
 #[wasm_bindgen]
 extern {
@@ -8,4 +16,19 @@ extern {
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn eval_script(line: &str) -> JsValue {
+    match Parser::new(line).prog() {
+        Ok(stmt) => {
+            match Eval::new().eval(&stmt) {
+                Ok(val) => val.to_string().into(),
+                Err(err) => err.to_string().into()
+            }
+        }
+        Err(err) => {
+            err.to_string().into()
+        }
+    }
 }
